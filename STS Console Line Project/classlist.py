5761cb8c2883b86d4362676e83c20451fd7cf11f
Edgar Go <goedgaraaron@gmail.com>
2025-12-15 16:19:07 +0800

    added enemyencounter, enemy, event, and restsite class

--- STS Console Line Project/classlist.py
+++ STS Console Line Project/classlist.py
@@ -28,5 +28,40 @@ class Character:
     def receive_status():
         print()
 
+class EnemyEncounter:
+    def __init__(self, id, name, type, enemy):
+        self.id = id
+        self.name = name
+        self.type = type
+        self.enemy = enemy # List of enemies
+
+class Enemy:
+    def __init__(self, id, name, health, block, status, intent, moveset):
+        self.id = id
+        self.name = name
+        self.health = health
+        self.block = block
+        self.status = status
+        self.intent = intent
+        self.moveset = moveset
+
+class Event:
+    def __init__(self, id, name, choices):
+        self.id = id
+        self.name = name
+        self.choices = choices
+
+# class Shop:
+#     def __init__(self, id, name):
+#         self.id = id
+#         self.name = name
+
+class RestSite:
+    def __init__(self):
+        self.choices = ["Rest", "Upgrade"]
+
+
+
+
 
 
